1471ec674234c678aed6a6331a8f1f271be8dcc6
Becatric <115936882+Becatric@users.noreply.github.com>
2023-10-21 16:24:46 +0300

    Add files via upload
    --funtii matematice

---
+++ Funtii.cpp
@@ -0,0 +1,50 @@
+#include<iostream> 
+#include<cmath>
+#define PI 3.1415926535897932384626
+
+using namespace std;
+
+double DeJongFuntion(int dimensiuni, vector<double>&x)
+{
+    double sum=0;
+
+    for (int i = 0; i < dimensions; i++)
+        sum += x[i] * x[i];
+
+    return sum;
+}
+
+double SchwefelFuntion(int dimensiuni, vector<double>&x)
+{
+    double sum=0;
+
+    for (int i = 0; i < dimensions; i++)
+        sum -= x[i] * sin(sqrt(abs(x[i])));
+
+    return 418.9829 * dimensions + sum;
+}
+
+double RastringinFuntion(int dimensiuni, vector<double>&x)
+{
+    double sum=10*dimenisiuni;
+
+    for (int i = 0; i < dimensions; i++)
+     sum += x[i] * x[i] - 10 * cos(2 * PI * x[i]);
+
+    return sum;
+}
+
+double MichalewiczFuntion(int dimensiuni, vector<double>&x)
+{ 
+    double sum=0;
+    int m=10;
+
+    for (int i = 0; i < dimensions; i++)
+        sum -= sin(x[i]) * pow(sin((i + 1) * x[i] * x[i] / PI), 2 * m);
+
+    return sum;
+}
+
+
+int main()
+{return 0;}
