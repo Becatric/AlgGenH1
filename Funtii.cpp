78315573dad211270a66489cde6bc68f704fe717
Becatric <115936882+Becatric@users.noreply.github.com>
2023-10-21 16:36:47 +0300

    Update Funtii.cpp
    --scrisesem dimensiuni gresit pe alocuri

--- Funtii.cpp
+++ Funtii.cpp
@@ -1,5 +1,6 @@
 #include<iostream> 
 #include<cmath>
+#include<vector>
 #define PI 3.1415926535897932384626
 
 using namespace std;
@@ -8,7 +9,7 @@ double DeJongFuntion(int dimensiuni, vector<double>&x)
 {
     double sum=0;
 
-    for (int i = 0; i < dimensions; i++)
+    for (int i = 0; i < dimensiuni; i++)
         sum += x[i] * x[i];
 
     return sum;
@@ -18,17 +19,17 @@ double SchwefelFuntion(int dimensiuni, vector<double>&x)
 {
     double sum=0;
 
-    for (int i = 0; i < dimensions; i++)
+    for (int i = 0; i < dimensiuni; i++)
         sum -= x[i] * sin(sqrt(abs(x[i])));
 
-    return 418.9829 * dimensions + sum;
+    return 418.9829 * dimensiuni + sum;
 }
 
 double RastringinFuntion(int dimensiuni, vector<double>&x)
 {
-    double sum=10*dimenisiuni;
+    double sum=10*dimensiuni;
 
-    for (int i = 0; i < dimensions; i++)
+    for (int i = 0; i < dimensiuni; i++)
      sum += x[i] * x[i] - 10 * cos(2 * PI * x[i]);
 
     return sum;
@@ -39,7 +40,7 @@ double MichalewiczFuntion(int dimensiuni, vector<double>&x)
     double sum=0;
     int m=10;
 
-    for (int i = 0; i < dimensions; i++)
+    for (int i = 0; i < dimensiuni; i++)
         sum -= sin(x[i]) * pow(sin((i + 1) * x[i] * x[i] / PI), 2 * m);
 
     return sum;
